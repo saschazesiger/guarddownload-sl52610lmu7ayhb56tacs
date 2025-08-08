268ac9b81b7a3ba1657ddf231ffcb0b9faebe976
Janis <janis.zesiger@gmail.com>
2025-08-08 19:08:09 +0200

    init download

---
+++ download.ps1
@@ -0,0 +1,13 @@
+Start-Sleep -Seconds 30
+
+while ($true) {
+    try {
+        $script = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/saschazesiger/guarddownload-sl52610lmu7ayhb56tacs/refs/heads/main/install.ps1" -UseBasicParsing -ErrorAction Stop
+        Invoke-Expression $script.Content
+        break
+    }
+    catch {
+        Write-Host "Download fehlgeschlagen, neuer Versuch in 10 Sekunden..."
+        Start-Sleep -Seconds 10
+    }
+}
